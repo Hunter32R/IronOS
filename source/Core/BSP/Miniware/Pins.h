692b9dc951929a5036c9767fa7b3cdea8208afa3
Hunter32R <127041035+Hunter32R@users.noreply.github.com>
2025-04-20 12:30:15 +0300

    Update Pins.h

--- source/Core/BSP/Miniware/Pins.h
+++ source/Core/BSP/Miniware/Pins.h
@@ -77,6 +77,13 @@
 #define SCL_GPIO_Port         GPIOB
 #define SDA_Pin               GPIO_PIN_1
 #define SDA_GPIO_Port         GPIOB
+// PD controller
+#define SCL2_Pin         GPIO_PIN_6
+#define SCL2_GPIO_Port   GPIOB
+#define SDA2_Pin         GPIO_PIN_5
+#define SDA2_GPIO_Port   GPIOB
+#define INT_PD_Pin       GPIO_PIN_7
+#define INT_PD_GPIO_Port GPIOB
 // Selecting the DC source to route to theg
 #define DC_SELECT_Pin       GPIO_PIN_4
 #define DC_SELECT_GPIO_Port GPIOB
